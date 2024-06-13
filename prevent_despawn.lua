7246d482169d5aadee2e613bb752b90425227d04
Benny <benny.bornemann@gmx.de>
2024-06-13 22:16:38 +0200

    Add files via upload

--- prevent_despawn.lua
+++ prevent_despawn.lua
@@ -1,48 +1,6 @@
 
--- Main thread to prevent vehicle despawn
-Citizen.CreateThread(function()
-    while true do
-        -- Wait 10 seconds between each check to reduce performance impact
-        Citizen.Wait(10000)
-
-        -- Get all vehicles in the world
-        local vehicles = GetAllVehicles()
-
-        for _, vehicle in ipairs(vehicles) do
-            -- Check if the vehicle exists
-            if DoesEntityExist(vehicle) then
-                -- Prevent the vehicle from despawning
-                SetEntityAsMissionEntity(vehicle, true, true)
-            end
-        end
-    end
-end)
-
--- Check if the player is using no-clip or spectating and prevent vehicle despawn
-Citizen.CreateThread(function()
-    while true do
-        -- Wait 1 second between each check to reduce performance impact
-        Citizen.Wait(1000)
-
-        local playerPed = PlayerPedId()
-        if IsPedInAnyVehicle(playerPed, false) then
-            local vehicle = GetVehiclePedIsIn(playerPed, false)
-            if DoesEntityExist(vehicle) then
-                -- Prevent the vehicle from despawning
-                SetEntityAsMissionEntity(vehicle, true, true)
-            end
-        else
-            local specTarget = NetworkIsInSpectatorMode()
-            if specTarget then
-                local vehicle = GetVehiclePedIsIn(GetPlayerPed(GetPlayerFromServerId(specTarget)), false)
-                if DoesEntityExist(vehicle) then
-                    -- Prevent the vehicle from despawning
-                    SetEntityAsMissionEntity(vehicle, true, true)
-                end
-            end
-        end
-    end
-end)
+-- Table to store vehicles that shouldn't despawn
+local savedVehicles = {}
 
 -- Function to get all vehicles in the world
 function GetAllVehicles()
@@ -58,3 +16,58 @@ function GetAllVehicles()
     EndFindVehicle(handle)
     return vehicles
 end
+
+-- Function to prevent vehicle from despawning
+function PreventVehicleDespawn(vehicle)
+    if DoesEntityExist(vehicle) and not savedVehicles[vehicle] then
+        SetEntityAsMissionEntity(vehicle, true, true)
+        savedVehicles[vehicle] = true
+    end
+end
+
+-- Main thread to prevent all vehicles from despawning
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(10000) -- Wait 10 seconds between each check to reduce performance impact
+
+        local vehicles = GetAllVehicles()
+        for _, vehicle in ipairs(vehicles) do
+            PreventVehicleDespawn(vehicle)
+        end
+    end
+end)
+
+-- Check if the player is using no-clip or spectating and prevent vehicle despawn
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000) -- Wait 1 second between each check to reduce performance impact
+
+        local playerPed = PlayerPedId()
+        if IsPedInAnyVehicle(playerPed, false) then
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
+            PreventVehicleDespawn(vehicle)
+        else
+            local specTarget = NetworkIsInSpectatorMode()
+            if specTarget then
+                local targetPed = GetPlayerPed(GetPlayerFromServerId(specTarget))
+                if IsPedInAnyVehicle(targetPed, false) then
+                    local vehicle = GetVehiclePedIsIn(targetPed, false)
+                    PreventVehicleDespawn(vehicle)
+                end
+            end
+        end
+    end
+end)
+
+-- Remove from savedVehicles table if vehicle is deleted
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(10000) -- Check every 10 seconds
+
+        for vehicle in pairs(savedVehicles) do
+            if not DoesEntityExist(vehicle) then
+                savedVehicles[vehicle] = nil
+            end
+        end
+    end
+end)
